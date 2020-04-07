f7167b47fd2b525387fb580cfd6c686c61f24442
Harris <info@instylegraphics.com>
2020-04-07 06:00:41 +0000

    caster live countdown timer 5min scene

--- echoproject/frontend/src/components/caster/casterView.js
+++ echoproject/frontend/src/components/caster/casterView.js
@@ -1,62 +1,103 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import ReactCountdownClock from "react-countdown-clock";
+import { setIntervalAsync } from 'set-interval-async/dynamic';
+import { clearIntervalAsync } from 'set-interval-async';
 
 export class CasterView extends Component {
 
+  constructor(props) {
+    super(props); 
+    this.state = {showImage: false};
+  }
+  
+  showBackgroundImage = () => {
+    this.setState({showImage: true});
+  };
+  
+  hideBackgroundImage = () => {
+    this.setState({showImage: false});
+  }; 
+ 
+    
   static propTypes = {
  
   };
     
   componentDidMount() {
-    function setTimer( timeinminutes ){        
-      var setminutes = 60 * timeinminutes;
-      var display = document.querySelector('#timer');
-      startTimer(setminutes, display);
-    }
-    
-    setInterval( setTimer( 5 ), 3000);
-    console.log("timer");
+   /*
+        this.timer = setIntervalAsync( 
+          async () => {
+           console.log('showBackgroundImage start');
+           this.showBackgroundImage },2500
+        );
+*/     
   }
     
+  componentWillUnmount() {
+/*
+    clearIntervalAsync(this.timer);
+    this.timer = null;
 
+    console.log('showBackgroundImage end');
+*/
+  };
+
+       
   render() {
+     const showImage = this.state.showImage;
+    
     //const { tournament, name, series_order, team_one, team_two, best_of } = this.state;
 
-/*   
-    document.getElementById("imgview").style.display = 'none';
-    document.getElementById("countdown").style.display = 'none';
-             
-    var hidden = false;
-    setInterval(function(){
-    document.getElementById("imgview").style.display = "none" ? "block" : "none";
-    document.getElementById("countdown").style.display = "none" ? "block" : "none";
-    hidden = !hidden;}, 2700);
-      
 
-*/    
-    return (
-      <div className="card card-body mt-4 mb-4">
-        <h2>Caster Live</h2>
+    const step = null;
   
+
+    switch (step) {
+      case 1:
+        return (
+         <div><p>case 1</p></div>
+        );
+      case 2:
+        return (
+           <div><p>case 1</p></div>
+        );
+      default :
+        //console.log("timer");
+        //setInterval( this.showBackgroundImage, 3000);
+        //this.timer = setIntervalAsync( this.showBackgroundImage, 2500);
+        setTimeout( this.showBackgroundImage, 3000);
+
+        return (
         <div className="scene">  
           <video id="videomain" className="scene--video" playsinline="playsinline" autoplay="true" controls="true" poster="./static/img/scenes/brb.jpg" >
               <source src={ './static/videos/TFLArmajet_Transition.webm' } type="video/webm"></source>
           </video>
-          <img id="imgview" className="scene--image" src={'./static/img/scenes/brb.jpg'} alt="Scene Desk"></img>
+          { showImage ? 
+          <div>
+          <img id="imgview" className="scene--image" src={'./static/img/scenes/brb.jpg'} alt="Scene"></img>
           <div className="container h-100">
             <div className="d-flex h-100 text-center align-items-center">
-              <div id="countdown" className="scene--content">
-                <h1>COUNTDOWN</h1>
-                <h1><span id="timer"></span> minutes!</h1>
+              <div id="countdown" className="scene--content__timer5min">
+                  <ReactCountdownClock seconds={300}
+                     color="#e00e11"
+                     alpha={1.0}
+                     size={300}
+                     />
               </div>
             </div>
           </div>
-        </div> 
-
-  
-      </div>
-    );
+          </div>
+          :
+          <p></p>
+          }
+          
+        </div>
+        );
+    }
+    
+    
   }
 }
 
